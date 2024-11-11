18e2433b047e48927f5391eb7adbbe98a4c67e57
sergiov-ni <sergio.velderrain@ni.com>
2024-11-11 13:16:22 -0600

    Builds 32 or 64 bit of the PPL using LabVIEW CLI on batch scripts
    (cherry picked from commit 5cab755db774390ed090516f5b094bed40098ab6)

---
+++ Tooling/deployment/build lv_icon pplx64.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Batch file to run multiple LabVIEW CLI commands in series
+
+REM Build 64 bit windows variant
+REM LabVIEWCLI -OperationName RunVI -VIPath "C:\labview-icon-editor\Tooling\deployment\Build the ppl.vi" -LabVIEWPath "C:\Program Files\National Instruments\LabVIEW 2021\LabVIEW.exe" -PortNumber 3366 -LogFilePath "C:\temp\log.txt" -Verbosity Detailed -LogToConsole true
+LabVIEWCLI -OperationName ExecuteBuildSpec -ProjectPath "C:\labview-icon-editor\lv_icon_editor.lvproj" -LabVIEWPath "C:\Program Files\National Instruments\LabVIEW 2021\LabVIEW.exe" -PortNumber 3366 -LogFilePath "C:\Users\Public\CLIlog.txt" -Verbosity Detailed -LogToConsole true -TargetName "My Computer" -BuildSpecName "Editor Packed Library"
+LabVIEWCLI -OperationName CloseLabVIEW
+REM Build 32 bit windows variant
+
+echo "All LabVIEW CLI commands completed successfully."
