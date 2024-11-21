e18a412f99151fe28934c5eabb6da7b065656eaf
sergiov-ni <sergio.velderrain@ni.com>
2024-11-20 21:50:35 -0800

    Simplified process so that we use a single batch file

--- Tooling/deployment/Build.bat
+++ Tooling/deployment/Build.bat
@@ -1,6 +1,7 @@
 REM  Run this batch file in admin mode since C:\Program Files\NI\LVAddon requires admin rights
 @echo on
-set "LVVersion=2021"
+REM DO NOT CHANGE LVVersion, since we need the PPL to be built on LabVIEW 2021
+set "LVVersion=2021" 
 set "SupportedBitness=64"
 set "ApiVersion=v1"
 set "MinimumSupportedLVVersion=23.0"
@@ -10,6 +11,7 @@ set "BuildSpec=Editor Packed Library"
 set "NIPB_Path=%RelativePath%\Tooling\deployment\NIPackage\IconEditorDeployment_x%SupportedBitness%.pbs"
 set "Project=%RelativePath%\lv_icon_editor.lvproj"
 set "PackedProjectLibraryVersion=1.0.1.0"
+REM Change the variable below to build VI Packages in other versions, or add additional lines to the batch file the build other versions to avoid having to build multiple times the PPL.
 set "VIPackageLabVIEWVersion=2021"
 
 REM Delete any previously built LV Addons
@@ -29,11 +31,10 @@ REM Create JSON file on LVAddon
 call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\NIPackage\CreateLVAddonJSONfile.vi" -- "niiconeditor%SupportedBitness%" "%ApiVersion%" "%MinimumSupportedLVVersion%" "%SupportedBitness%" 
 REM Build VI Package
 call g-cli --lv-ver %VIPackageLabVIEWVersion% --arch %SupportedBitness% -v vipb -- -av -b "%RelativePath%\Tooling\deployment\VIPackage\NI Icon editor.vipb"
-REM Removes token LocalHost.LibraryPaths from LabVIEW.ini
 REM Build NI Package
 REM cd /d C:\Program Files\National Instruments\Package Builder
 REM call nipbcli -o="%NIPB_Path%" -b=packages -save
-REM call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\NIPackage\DestroyLVINILocalHostKey.vi"
+REM Removes token LocalHost.LibraryPaths from LabVIEW.ini
+ call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\NIPackage\DestroyLVINILocalHostKey.vi"
 REM Quit LabVIEW
 call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% QuitLabVIEW
-pause
