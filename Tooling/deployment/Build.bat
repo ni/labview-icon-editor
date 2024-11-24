054be8308cfd92cbef21e90c0497efc3bbe4a1a6
sergiov-ni <sergio.velderrain@ni.com>
2024-11-23 20:34:08 -0800

    Bugfix: VIPB now gets switched to the labview version you are trying to build, since previous version was leaving rogue labview executions open. Has been tested on LV2024

--- Tooling/deployment/Build.bat
+++ Tooling/deployment/Build.bat
@@ -1,7 +1,7 @@
 REM  Run this batch file in admin mode since C:\Program Files\NI\LVAddon requires admin rights
 @echo on
 REM DO NOT CHANGE LVVersion, since we need the PPL to be built on LabVIEW 2021
-set "LVVersion=2021" 
+set "PPL_LVVersion=2021" 
 set "SupportedBitness=64"
 set "ApiVersion=v1"
 set "MinimumSupportedLVVersion=23.0"
@@ -12,42 +12,64 @@ set "NIPB_Path=%RelativePath%\Tooling\deployment\IconEditorDeployment_x%Supporte
 set "Project=%RelativePath%\lv_icon_editor.lvproj"
 set "PackedProjectLibraryVersion=1.0.1.0"
 REM Change the variable below to build VI Packages in other versions, or add additional lines to the batch file the build other versions to avoid having to build multiple times the PPL.
-set "VIPackageLabVIEWVersion=2021"
+REM set "VIP_LVVersion_B="24.3"
+set "VIP_LVVersion_B="24.3 (64-bit)""
+set "VIP_LVVersion_A=2024"
 
-REM Apply dependencies
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v vipc -- %RelativePath%\Tooling\deployment\Dependencies.vipc
+REM Removes token LocalHost.LibraryPaths from LabVIEW.ini
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\Destroy_LV_INI_Token.vi" -- LabVIEW localhost.LibraryPaths
+IF %ERRORLEVEL% NEQ 0 goto end
+
+REM Quit LabVIEW
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% QuitLabVIEW
+IF %ERRORLEVEL% NEQ 0 goto end
+
+REM Apply dependencies 1
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v vipc -- %RelativePath%\Tooling\deployment\Dependencies.vipc
+IF %ERRORLEVEL% NEQ 0 goto end
+
+REM Modify VIPB to have a different labview version
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\Modify_VIPB_LabVIEW_Version.vi" -- "%RelativePath%\Tooling\deployment\NI Icon editor.vipb" %VIP_LVVersion_B%
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Add Localhost.LibraryPaths token to LabVIEW INI
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\Create_LV_INI_Token.vi" -- LabVIEW Localhost.LibraryPaths "%RelativePath%"
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\Create_LV_INI_Token.vi" -- LabVIEW Localhost.LibraryPaths "%RelativePath%"
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Quit LabVIEW
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% QuitLabVIEW
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% QuitLabVIEW
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Zips and moves lv_icon.lvlibp as well as LabVIEW Icon API from VI Lib to a different location
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\PrepareIESource.vi" -- "%Project%" "Editor Packed Library"
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\PrepareIESource.vi" -- "%Project%" "Editor Packed Library"
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Quit LabVIEW
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% QuitLabVIEW
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% QuitLabVIEW
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Run Unit tests
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\Run all tests CLI.vi"
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\Run all tests CLI.vi"
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Build the PPL
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% lvbuildspec -- -v %PackedProjectLibraryVersion% -p "%Project%" -b "%BuildSpec%"
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% lvbuildspec -- -v %PackedProjectLibraryVersion% -p "%Project%" -b "%BuildSpec%"
 IF %ERRORLEVEL% NEQ 0 goto end
 
-REM Build VI Package
-call g-cli --lv-ver %VIPackageLabVIEWVersion% --arch %SupportedBitness% -v vipb -- -av -b "%RelativePath%\Tooling\deployment\NI Icon editor.vipb"
+REM Switch VIPM target
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\Deployment\Switch_VIPM_Target.vi" -- %VIP_LVVersion_B%
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Quit LabVIEW
-IF %LVVersion% NEQ %VIPackageLabVIEWVersion% call g-cli --lv-ver %VIPackageLabVIEWVersion% --arch %SupportedBitness% QuitLabVIEW 
+call g-cli --lv-ver %PPL_LVVersion% --arch %SupportedBitness% QuitLabVIEW
+IF %ERRORLEVEL% NEQ 0 goto end
+
+REM Build VI Package
+call g-cli --lv-ver %VIP_LVVersion_A% --arch %SupportedBitness% -v vipb -- -av -b "%RelativePath%\Tooling\deployment\NI Icon editor.vipb"
+IF %ERRORLEVEL% NEQ 0 goto end
+
+REM Quit LabVIEW
+call g-cli --lv-ver %VIP_LVVersion_A% --arch %SupportedBitness% QuitLabVIEW
 IF %ERRORLEVEL% NEQ 0 goto end
 
 REM Build NI Package
@@ -55,11 +77,6 @@ REM cd /d C:\Program Files\National Instruments\Package Builder
 REM call nipbcli -o="%NIPB_Path%" -b=packages -save
 REM IF %ERRORLEVEL% NEQ 0 goto end
 
-REM Removes token LocalHost.LibraryPaths from LabVIEW.ini
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% -v "%RelativePath%\Tooling\deployment\Destroy_LV_INI_Token.vi.vi" -- LabVIEW Localhost.LibraryPaths
-
-REM Quit LabVIEW
-call g-cli --lv-ver %LVVersion% --arch %SupportedBitness% QuitLabVIEW
 :end 
 IF %ERRORLEVEL% NEQ 0 pause
-pause
+
