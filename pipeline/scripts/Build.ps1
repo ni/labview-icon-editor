9e1b773fe39632d89719c06db1fae99fedef83d1
sergiov-ni <sergio.velderrain@ni.com>
2024-11-28 10:29:48 -0600

    Automated build process using a powershell script

--- pipeline/scripts/Build.ps1
+++ pipeline/scripts/Build.ps1
@@ -1,16 +1,20 @@
-
-#Example: .\Build.ps1 -MinimumSupportedLVVersion "2021" -SupportedBitness "64" -RelativePath "C:\labview-icon-editor"
+# Example usage:
+# .\build.ps1 -MinimumSupportedLVVersion "2021" -SupportedBitness "64" -RelativePath "C:\labview-icon-editor" -LabVIEW_Project "lv_icon_editor" -VIPBPath "Tooling\deployment\NI Icon editor.vipb" -VIP_LVVersion "2024"
 param(
     [string]$MinimumSupportedLVVersion,
     [string]$SupportedBitness,
-    [string]$RelativePath
+    [string]$RelativePath,
+    [string]$LabVIEW_Project,
+    [string]$VIPBPath,
+    [string]$VIP_LVVersion
 )
 
 # Construct the command
 $script = @"
 .\RunUnitTests.ps1 -MinimumSupportedLVVersion "$MinimumSupportedLVVersion" -SupportedBitness "$SupportedBitness" -RelativePath "$RelativePath"
-.\Build_lvlibp.ps1 -MinimumSupportedLVVersion "$MinimumSupportedLVVersion" -SupportedBitness "$SupportedBitness" -RelativePath "C:\labview-icon-editor"
-.\Close_LabVIEW.ps1 -MinimumSupportedLVVersion "$MinimumSupportedLVVersion" -SupportedBitness "$SupportedBitness" QuitLabVIEW
+.\Build_lvlibp.ps1 -MinimumSupportedLVVersion "$MinimumSupportedLVVersion" -SupportedBitness "$SupportedBitness" -RelativePath "$RelativePath"
+.\Build_vip.ps1 -MinimumSupportedLVVersion "$MinimumSupportedLVVersion" -SupportedBitness "$SupportedBitness" -RelativePath "$RelativePath" -LabVIEW_Project "$LabVIEW_Project" -VIPBPath "$VIPBPath" -VIP_LVVersion "$VIP_LVVersion"
+
 "@
 
 Write-Output "Executing the following command:"
