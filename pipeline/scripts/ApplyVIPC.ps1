d3d579031da2dad9ad2b6b371f19c65afb584e9d
sergiov-ni <sergio.velderrain@ni.com>
2024-12-03 17:37:16 -0600

    Paths that contained spaced were making the script to fail, fixed it.

--- pipeline/scripts/ApplyVIPC.ps1
+++ pipeline/scripts/ApplyVIPC.ps1
@@ -1,6 +1,5 @@
 # Example Usage:
 # .\Applyvipc.ps1 -MinimumSupportedLVVersion "2021" -SupportedBitness "64" -RelativePath "C:\labview-icon-editor" -VIPCPath "Tooling\deployment\Dependencies.vipc" -VIP_LVVersion "2021"
-
 param (
     [string]$MinimumSupportedLVVersion,
     [string]$VIP_LVVersion,
