f4d1c1928c0252201c6609ad95df6d1875694abf
Sergio Velderrain <sergio.velderrain@emerson.com>
2025-03-12 16:09:28 -0700

    Always try to apply dependencies twice to take into account the first failed execution when there is a very big initial VIPC

--- pipeline/scripts/ApplyVIPC.ps1
+++ pipeline/scripts/ApplyVIPC.ps1
@@ -79,12 +79,14 @@ Write-Output "Applying dependencies for LabVIEW $VIP_LVVersion_B..."
 # First, always apply dependencies for the “minimum supported” version
 $script = @"
 g-cli --lv-ver $MinimumSupportedLVVersion --arch $SupportedBitness -v "$($ResolvedRelativePath)\Tooling\Deployment\Applyvipc.vi" -- "$ResolvedVIPCPath" "$VIP_LVVersion_B"
+g-cli --lv-ver $MinimumSupportedLVVersion --arch $SupportedBitness -v "$($ResolvedRelativePath)\Tooling\Deployment\Applyvipc.vi" -- "$ResolvedVIPCPath" "$VIP_LVVersion_B"
 "@
 
 # If we need to switch to a different version, we’ll quit LabVIEW and re-apply
 if ($VIP_LVVersion -ne $MinimumSupportedLVVersion) {
     $script += @"
 g-cli --lv-ver $MinimumSupportedLVVersion --arch $SupportedBitness -v QuitLabVIEW
+g-cli --lv-ver $VIP_LVVersion --arch $SupportedBitness -v "$($ResolvedRelativePath)\Tooling\Deployment\Applyvipc.vi" -- "$ResolvedVIPCPath" "$VIP_LVVersion_A"
 g-cli --lv-ver $VIP_LVVersion --arch $SupportedBitness -v "$($ResolvedRelativePath)\Tooling\Deployment\Applyvipc.vi" -- "$ResolvedVIPCPath" "$VIP_LVVersion_A"
 "@
 }
